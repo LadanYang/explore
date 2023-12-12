5da05cdc2a4715e13c9ee8026cfdfdfec54de0f1
Ladan Yang <cyang93@smith.edu>
2023-12-12 17:13:04 -0500

    updated description

--- R/raster.R
+++ R/raster.R
@@ -1,11 +1,11 @@
-raster <- function(spikes,eventstart,varargin) {
 
-}
-
-new_single_cell <- function(x, xaxis) {
+#'Given a matlab data set, this function creates a dataframe
+#'@param matdat the matlab data with the first column neurons firing time/location, eg. the file path to a matlab file
+#'@export
+new_single_cell <- function(x) {
 
   stopifnot(is.data.frame(x))
-  stopifnot(is.character(xaxis))
+  #stopifnot(is.character(xaxis))
 
   structure(x,
             "class" = "single_cell"
@@ -16,13 +16,15 @@ validate_single_cell <- function() {
 
 }
 
+#' @param matdat the matlab data with the first column neurons firing time/location, eg. the file path to a matlab file
+#' @export
 #' @importFrom R.matlab readMat
-single_cell <- function(matdata, xaxis){
+single_cell <- function(matdata){
 
   dfdata<-as.data.frame(readMat(matdata))
   colnames(dfdata) <-c("V1","V2")
 
-  obj<-new_single_cell(dfdata,xaxis
+  obj<-new_single_cell(dfdata
 
                   )
   #obj<-validate_single_cell(obj)
