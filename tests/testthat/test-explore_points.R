db52a5ebd18b437292246623f1b880b4ec35988d
Ari Cross <across@smith.edu>
2023-12-19 17:00:45 -0600

    finished test files for explore_polygons and explore_points

--- tests/testthat/test-explore_points.R
+++ tests/testthat/test-explore_points.R
@@ -1,5 +1,13 @@
-test_that("geometry is point", {
-  expect_equal(2 * 2, 4)
-})
+test_that("Test if geometry is of class 'POINT'", {
+  # Create or load an 'sf' object for testing
+  your_sf_object <- ...  # Replace with your data or function call
 
+  # Call your function
+  result <- explore_points(your_sf_object)  # Replace with the actual function name
+
+  # Check the result using expect_true or expect_false
+  expect_true(result, "The geometry is of class 'POINT'")
+  # OR
+  # expect_false(result, "The geometry is NOT of class 'POINT'")
+})
 
--- tests/testthat/test-explore_polygons.R
+++ tests/testthat/test-explore_polygons.R
@@ -1,4 +1,19 @@
-test_that("multiplication works", {
+test_that("geometry is polygon", {
   expect_equal(2 * 2, 4)
 })
 
+library(testthat)
+library(explore)  # Replace 'your_package_name' with the actual name of your package
+
+test_that("Test if geometry is of class 'POLYGON' or 'MULTIPOLYGON'", {
+  # Create or load an 'sf' object for testing
+  your_sf_object <- ...  # Replace with your data or function call
+
+  # Call your function
+  result <- explore_polygons(your_sf_object)  # Replace with the actual function name
+
+  # Check the result using expect_true or expect_false
+  expect_true(result, "The geometry is of class 'POLYGON' or 'MULTIPOLYGON'")
+  # OR
+  # expect_false(result, "The geometry is NOT of class 'POLYGON' or 'MULTIPOLYGON'")
+})
