a8bac130af22f2961e069ef05cdf90a945ff52f4
Ari Cross <across@smith.edu>
2023-12-20 18:22:15 -0600

    taking out params??

--- R/explore.R
+++ R/explore.R
@@ -131,11 +131,6 @@ explore_points <- function(data, variable = NULL) {
 #'
 #' @param var_column optional variable to plot a scaled and/or gradient map
 #'
-#' @param base.map fixed OpenStreetMap base
-#'
-#' @param polygons user input 'sf' class
-#'
-#' @param plot_type plot the user desired to access
 #'
 #' @return A list of visualizations interactive visualizations accessible through exp. 'list$map'
 #'
